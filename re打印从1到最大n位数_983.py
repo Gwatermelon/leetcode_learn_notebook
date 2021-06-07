4df16543f64a16017fcf734537f23ed47217d68c
Zhang Ge <65587120+Gwatermelon@users.noreply.github.com>
2021-06-07 16:58:47 +0100

    大数问题

---
+++ re打印从1到最大n位数_983.py
@@ -0,0 +1,28 @@
+class Solution:
+    def printNumbers(self, n: int) -> List[int]:
+        end = 10**n -1
+        
+        return list(range(1,end+1))
+      
+ #考虑大数问题
+
+class Solution:
+    def printNumbers(self, n: int) -> [int]:
+        def dfs(x):
+            if x == n:
+                s = ''.join(num[self.start:])
+                if s != '0': res.append(s)
+                if n - self.start == self.nine: self.start -= 1
+                return
+            for i in range(10):
+                if i == 9: self.nine += 1
+                num[x] = str(i)
+                dfs(x + 1)
+            self.nine -= 1
+        
+        num, res = ['0'] * n, []
+        self.nine = 0
+        self.start = n - 1
+        dfs(0)
+        return ','.join(res)
+
