a0046e2131589631119374af4b9b4dd3df26b411
Zhang Ge <65587120+Gwatermelon@users.noreply.github.com>
2021-06-19 16:04:35 +0100

    Create 二叉搜索树的最近公共祖先_566.py

---
+++ 二叉搜索树的最近公共祖先_566.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+根据二叉树的最近公共祖先可直接得出如下
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q: return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if not left: return right
+        if not right: return left
+        return root
+
+#根据二叉搜索树的性质，可用如下
+ class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            if root.val > p.val and root.val > q.val:
+                root = root.left
+            elif root.val <p.val and root.val< q.val:
+                root = root.right
+            else: break
+
+        return root 
+            
